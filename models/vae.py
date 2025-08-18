dc25d2e71092732216d03d6f89d74d4d8b41a615
KeithanS <smith.keithan@gmail.com>
2025-08-18 12:10:32 -0400

    add vae prototype

---
+++ models/vae.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn as nn
+
+class TabularVAE(nn.Module):
+    def __init__(self, input_dim: int, latent_dim: int = 16, hidden: int = 128):
+        super().__init__()
+        self.enc = nn.Sequential(
+            nn.Linear(input_dim, hidden), nn.ReLU(),
+            nn.Linear(hidden, hidden), nn.ReLU()
+        )
+        self.mu = nn.Linear(hidden, latent_dim)
+        self.logvar = nn.Linear(hidden, latent_dim)
+        self.dec = nn.Sequential(
+            nn.Linear(latent_dim, hidden), nn.ReLU(),
+            nn.Linear(hidden, hidden), nn.ReLU(),
+            nn.Linear(hidden, input_dim)
+        )
+
+    def encode(self, x):
+        h = self.enc(x)
+        return self.mu(h), self.logvar(h)
+
+    def reparam(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparam(mu, logvar)
+        recon = self.dec(z)
+        return recon, mu, logvar
+
+    @staticmethod
+    def loss_fn(recon, x, mu, logvar):
+        recon_loss = nn.functional.mse_loss(recon, x, reduction='mean')
+        kld = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+        return recon_loss + 0.001*kld, {'recon': recon_loss.item(), 'kld': kld.item()}
